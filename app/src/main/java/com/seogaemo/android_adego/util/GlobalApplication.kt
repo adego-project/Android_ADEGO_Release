acb1b700bac07dd370934a0d28d49fad7af0af0e
JangWoojun <woojun0107@naver.com>
2024-07-12 06:40:32 +0900

    feat: GlobalApplication 추가 및 TokenManager 초기화 세팅

---
+++ app/src/main/java/com/seogaemo/android_adego/util/GlobalApplication.kt
@@ -0,0 +1,11 @@
+package com.seogaemo.android_adego.util
+
+import android.app.Application
+import com.seogaemo.android_adego.database.TokenManager
+
+class GlobalApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        TokenManager.init(this)
+    }
+}
