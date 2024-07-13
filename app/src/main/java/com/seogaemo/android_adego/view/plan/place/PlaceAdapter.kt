7b31576e1ab27fc3906b951dd0cd7e7ab80a22c7
JangWoojun <woojun0107@naver.com>
2024-07-14 01:09:18 +0900

    feat: PlaceAdapter 추가

---
+++ app/src/main/java/com/seogaemo/android_adego/view/plan/place/PlaceAdapter.kt
@@ -0,0 +1,62 @@
+package com.seogaemo.android_adego.view.plan.place
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.seogaemo.android_adego.data.Address
+import com.seogaemo.android_adego.databinding.PlaceItemBinding
+import com.seogaemo.android_adego.view.plan.PlanActivity
+
+
+class PlaceAdapter(private val addressList: List<Address>): RecyclerView.Adapter<PlaceAdapter.PlaceViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaceViewHolder {
+        val binding = PlaceItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PlaceViewHolder(binding).also { handler->
+            binding.root.setOnClickListener {
+                val activity = (binding.root.context as PlanActivity)
+                val item = addressList[handler.adapterPosition]
+
+                if (item.roadAddressName.isNotBlank() || item.addressName.isNotBlank()) {
+                    val address = item.roadAddressName.ifBlank { item.addressName }
+                    binding.locationDetailsName.text = address
+                    activity.apply {
+                        this.planPlace = addressList[handler.adapterPosition].placeName
+                        this.planAddress = address
+                        this.finishAffinity()
+                    }
+                }
+
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return addressList.size
+    }
+
+    override fun onBindViewHolder(holder: PlaceViewHolder, position: Int) {
+        holder.bind(addressList[position])
+    }
+
+    class PlaceViewHolder(private val binding: PlaceItemBinding):
+        ViewHolder(binding.root) {
+        fun bind(address: Address) {
+            binding.locationName.text = address.placeName
+
+            if (address.roadAddressName.isNotBlank() || address.addressName.isNotBlank()) {
+                val textToShow = if (address.roadAddressName.isNotBlank()) {
+                    address.roadAddressName
+                } else {
+                    address.addressName
+                }
+                binding.locationDetailsName.text = textToShow
+            } else {
+                binding.locationDetailsName.visibility = View.GONE
+            }
+        }
+    }
+
+}
