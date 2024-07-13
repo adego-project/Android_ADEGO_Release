fd14211ac8002ab6e88053e3f83e91bfd9a3c11d
JangWoojun <woojun0107@naver.com>
2024-07-14 01:10:52 +0900

    feat: PlanFinishFragment 로직 구현
    버튼 action 및 binding 처리

--- app/src/main/java/com/seogaemo/android_adego/view/plan/PlanFinishFragment.kt
+++ app/src/main/java/com/seogaemo/android_adego/view/plan/PlanFinishFragment.kt
@@ -1,11 +1,19 @@
 package com.seogaemo.android_adego.view.plan
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import com.seogaemo.android_adego.R
 import com.seogaemo.android_adego.databinding.FragmentPlanFinishBinding
+import com.seogaemo.android_adego.view.main.MainActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class PlanFinishFragment : Fragment() {
@@ -27,6 +35,45 @@ class PlanFinishFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val activity = requireActivity() as PlanActivity
+        val time = activity.planTime.split(":")
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val isSuccess = activity.setPlan(requireContext()) != null
+            withContext(Dispatchers.Main) {
+                if (isSuccess) {
+                    activity.findViewById<LinearLayout>(R.id.back_button).visibility = View.GONE
+
+                    binding.dateText.text = activity.planDate
+                    binding.timeText.text = if (time[0].toInt() > 11) {
+                        "오후 ${time[0].toInt() % 12}시 ${time[1]}분"
+                    } else {
+                        "오전 ${time[0]}시 ${time[1]}분"
+                    }
+                    binding.locationText.text = activity.planPlace
+
+                    binding.sharedButton.setOnClickListener {
+                        startActivity(
+                            Intent(Intent.ACTION_SEND_MULTIPLE).apply {
+                                type = "text/plain"
+                                val url = ""
+                                val content = "약속에 초대됐어요!\n하단 링크를 통해 어떤 약속인지 확인하세요."
+                                putExtra(Intent.EXTRA_TEXT,"$content\n\n$url")
+                            }
+                        )
+                    }
+
+                    binding.finishButton.setOnClickListener {
+                        startActivity(Intent(requireActivity(), MainActivity::class.java))
+                        requireActivity().finishAffinity()
+                    }
+                } else {
+                    startActivity(Intent(requireActivity(), MainActivity::class.java))
+                    requireActivity().finishAffinity()
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
