9e2b0e4d87b9e15491511354b0aed703863de1a9
JangWoojun <woojun0107@naver.com>
2024-07-14 01:07:47 +0900

    feat: PlanNameFragment 로직 구현
    nameInput 처리 및 버튼 처리

--- app/src/main/java/com/seogaemo/android_adego/view/plan/PlanNameFragment.kt
+++ app/src/main/java/com/seogaemo/android_adego/view/plan/PlanNameFragment.kt
@@ -1,10 +1,15 @@
 package com.seogaemo.android_adego.view.plan
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity.INPUT_METHOD_SERVICE
 import com.seogaemo.android_adego.databinding.FragmentPlanNameBinding
 
 class PlanNameFragment : Fragment() {
@@ -26,6 +31,44 @@ class PlanNameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.nameInput.apply {
+            this.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    binding.nameLengthText.text = "약속 이름 (${binding.nameInput.text.length}/12)"
+                    if (binding.nameInput.text.isNotEmpty()) {
+                        binding.nextButton.visibility = View.VISIBLE
+                        binding.noneButton.visibility = View.GONE
+                    } else {
+                        binding.nextButton.visibility = View.GONE
+                        binding.noneButton.visibility = View.VISIBLE
+                    }
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                }
+            })
+            this.setOnEditorActionListener { _, actionId, _ ->
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    val manager = requireActivity().getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                    manager.hideSoftInputFromWindow(requireActivity().currentFocus!!.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+                    binding.nameInput.clearFocus()
+                    true
+                } else {
+                    false
+                }
+            }
+        }
+
+        binding.nextButton.setOnClickListener {
+            val activity = (requireActivity() as PlanActivity)
+
+            activity.planName = binding.nameInput.text.toString()
+            activity.addFragment(PlanDateFragment())
+        }
     }
 
     override fun onDestroyView() {
