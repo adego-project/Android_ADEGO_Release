6a16edbe2199ba1b2c4a5f6ec3b78bf2a4efbec0
JangWoojun <woojun0107@naver.com>
2024-07-13 21:59:33 +0900

    feat: Plan 관련 Activity, Fragment 추가

---
+++ app/src/main/java/com/seogaemo/android_adego/view/plan/PlanActivity.kt
@@ -0,0 +1,21 @@
+package com.seogaemo.android_adego.view.plan
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.seogaemo.android_adego.R
+
+class PlanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_plan)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
