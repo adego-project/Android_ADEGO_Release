6e1332572851a223e66a677d15f8026a0e7a246b
JangWoojun <woojun0107@naver.com>
2024-07-15 16:41:37 +0900

    feat: 서버 Response 변경에 따른 PlanResponse 속성 변경

--- app/src/main/java/com/seogaemo/android_adego/data/PlanResponse.kt
+++ app/src/main/java/com/seogaemo/android_adego/data/PlanResponse.kt
@@ -1,10 +1,12 @@
 package com.seogaemo.android_adego.data
 
 data class PlanResponse(
-    val createdAt: String,
-    val date: String,
     val id: String,
     val name: String,
     val place: Place,
-    val users: List<UserResponse>
+    val date: String,
+    val users: List<UserResponse>,
+    val isAlarmAvailable: Boolean,
+    val createdAt: String,
+    val status: String
 )
