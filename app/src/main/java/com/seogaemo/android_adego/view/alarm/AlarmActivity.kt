9787f0922c4f886fc3f4686d933142b0ce04b271
JangWoojun <woojun0107@naver.com>
2024-07-19 15:45:49 +0900

    feat: 알람 울리기 리스트에서 자신 제거

--- app/src/main/java/com/seogaemo/android_adego/view/alarm/AlarmActivity.kt
+++ app/src/main/java/com/seogaemo/android_adego/view/alarm/AlarmActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import com.seogaemo.android_adego.databinding.ActivityAlarmBinding
 import com.seogaemo.android_adego.util.Util.getPlan
+import com.seogaemo.android_adego.util.Util.getUser
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -23,11 +24,14 @@ class AlarmActivity : AppCompatActivity() {
 
         CoroutineScope(Dispatchers.IO).launch {
             val planResponse = getPlan(this@AlarmActivity)
+            val myId = getUser(this@AlarmActivity)?.id.toString()
+
             if (planResponse != null) {
+                val alarmList = planResponse.users.filter { it.id != myId }
                 withContext(Dispatchers.Main) {
                     binding.friendsList.apply {
                         this.layoutManager = GridLayoutManager(this@AlarmActivity, 2)
-                        this.adapter = AlarmAdapter(planResponse.users)
+                        this.adapter = AlarmAdapter(alarmList)
                     }
                 }
             }
