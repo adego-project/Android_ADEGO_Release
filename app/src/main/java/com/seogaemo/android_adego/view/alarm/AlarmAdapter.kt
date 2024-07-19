a2a06dfcdde8f86ad5503490af46c941428837bc
JangWoojun <woojun0107@naver.com>
2024-07-19 15:46:02 +0900

    feat: 알람 울리기 기능 구현 및 진동 구현

--- app/src/main/java/com/seogaemo/android_adego/view/alarm/AlarmAdapter.kt
+++ app/src/main/java/com/seogaemo/android_adego/view/alarm/AlarmAdapter.kt
@@ -1,20 +1,41 @@
 package com.seogaemo.android_adego.view.alarm
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
 import com.seogaemo.android_adego.data.UserResponse
+import com.seogaemo.android_adego.database.TokenManager
 import com.seogaemo.android_adego.databinding.AlarmItemBinding
+import com.seogaemo.android_adego.network.RetrofitAPI
+import com.seogaemo.android_adego.network.RetrofitClient
+import com.seogaemo.android_adego.util.Util
+import com.seogaemo.android_adego.view.auth.LoginActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class AlarmAdapter(private val userList: List<UserResponse>): RecyclerView.Adapter<AlarmAdapter.AlarmViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlarmViewHolder {
         val binding = AlarmItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val vibrator = binding.root.context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
         return AlarmViewHolder(binding).also { handler->
             binding.root.setOnClickListener {
-                callUser(userList[handler.adapterPosition])
+                val effect = VibrationEffect.createOneShot(100L, VibrationEffect.EFFECT_CLICK)
+                vibrator.vibrate(effect)
+
+                CoroutineScope(Dispatchers.IO).launch {
+                    callUser(binding.root.context, userList[handler.adapterPosition].id)
+                }
             }
         }
     }
@@ -40,8 +61,33 @@ class AlarmAdapter(private val userList: List<UserResponse>): RecyclerView.Adapt
         }
     }
 
-    private fun callUser(user: UserResponse) {
-
+    private suspend fun callUser(context: Context, id: String): Unit? {
+        return try {
+            withContext(Dispatchers.IO) {
+                val retrofitAPI = RetrofitClient.getInstance().create(RetrofitAPI::class.java)
+                val response = retrofitAPI.callUser("bearer ${TokenManager.accessToken}", id)
+                if (response.isSuccessful) {
+                    response.body()
+                } else if (response.code() == 401) {
+                    val getRefresh = Util.getRefresh()
+                    if (getRefresh != null) {
+                        TokenManager.refreshToken = getRefresh.refreshToken
+                        TokenManager.accessToken = getRefresh.accessToken
+                        callUser(context, id)
+                    } else {
+                        TokenManager.refreshToken = ""
+                        TokenManager.accessToken = ""
+                        context.startActivity(Intent(context, LoginActivity::class.java))
+                        (context as Activity).finishAffinity()
+                        null
+                    }
+                } else {
+                    null
+                }
+            }
+        } catch (e: Exception) {
+            null
+        }
     }
 
 }
